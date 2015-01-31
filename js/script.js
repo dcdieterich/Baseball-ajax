00228bc500a4dff036c79455d35c393ec73ce49d
Danielle Dieterich <danielledieterich@Danielles-MacBook-Pro-7.local>
2015-01-30 22:30:29 -0600

    simple ajax call with baseball card json

--- js/script.js
+++ js/script.js
@@ -1,9 +1,37 @@
+var playerData;
+
 $(document).ready(function() {
-    console.log("Hello world.")
+    getData();
 });
 
 
+function getData() {
+	$.getJSON("js/baseball_card.json", function(data, error){
+	playerData=data;
+	drawStuff();
+	});
 
+}
+
+function drawStuff() {
+
+var playerName=playerData.name;
+var playerTeam=playerData.club;
+var playerStats=playerData.stats;
+
+var lastSeasonIndex=playerStats.length -1;
+
+var lastSeasonStats=playerData.stats[lastSeasonIndex];
+
+var lastSeasonHomeRuns= lastSeasonStats.HR;
+var lastSeasonYear=lastSeasonStats.year;
+var lastSeasonTeam=lastSeasonStats.club;
+
+$(".chart").append("<h1>"+playerName+"</h1>");
+$(".chart").append("<h3>"+playerTeam+"</h3>");
+$(".chart").append("<p>In "+lastSeasonYear+", "+playerName+" hit "+lastSeasonHomeRuns+" home runs for the "+lastSeasonTeam);
+
+}
 
 
 
